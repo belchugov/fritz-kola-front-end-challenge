875c6981e3802d680204e228f3c61ec89e4a6699
Dimitar Belchugov <v-dbelchugov@expediagroup.com>
2022-01-20 16:20:07 +0200

    Fixed couple of lint errors and wornings

--- src/App.js
+++ src/App.js
@@ -1,76 +1,77 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import './App.css';
-import axios from "axios";
-import './Quontity'
-import Quontity from "./Quontity";
+import axios from 'axios';
+import Quontity from './Quontity';
 
 function App() {
-    const axios = require('axios').default;
-    const url = 'https://fritz-kola-challenge.s3-eu-west-1.amazonaws.com/products.json'
-    const [products, setProducts] = useState()
-    const [total, setTotal] = useState(0)
+  const axios = require('axios').default;
+  const url = 'https://fritz-kola-challenge.s3-eu-west-1.amazonaws.com/products.json';
+  const [products, setProducts] = useState();
+  const [total, setTotal] = useState(0);
 
-    const [cartItems, setCartItems] = useState([]);
-    const onAdd = (product) => {
-        const exist = cartItems.find((x) => x.name === product.name);
-        debugger
-        if (exist) {
-            setCartItems(
-                cartItems.map((x) =>
-                    x.name === product.name ? { ...exist, qty: exist.qty + 1 } : x
-                )
-            );
-        } else {
-            setCartItems([...cartItems, { ...product, qty: 1 }]);
-        }
-    };
-    const onRemove = (product) => {
-        const exist = cartItems.find((x) => x.id === product.id);
-        if (exist.qty === 1) {
-            setCartItems(cartItems.filter((x) => x.id !== product.id));
-        } else {
-            setCartItems(
-                cartItems.map((x) =>
-                    x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
-                )
-            );
-        }
-    };
+  const [cartItems, setCartItems] = useState([]);
+  const onAdd = (product) => {
+    const exist = cartItems.find((x) => x.name === product.name);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) => (x.name === product.name ? { ...exist, qty: exist.qty + 1 } : x)),
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  };
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) => (x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x)),
+      );
+    }
+  };
 
-    useEffect(() => {
-        axios({
-            method: 'get',
-            url: url,
-        })
-            .then((response) => {
-                setProducts(response.data.products)
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-    }, [axios])
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url,
+    })
+      .then((response) => {
+        setProducts(response.data.products);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [axios]);
 
-    const productElement = products?.map((product) =>
-        <div className='card' key={product.name}>
-            <img className='special-image' src={product.image} alt={product.name}/>
-            <div className='card-body'>
-                <span className='price'>
-                    {(product.price).toLocaleString('en-UK', {
-                        style: 'currency',
-                        currency: 'GBP'
-                    })}
-                </span>
-                <Quontity onAdd={onAdd} onRemove={onRemove} product={product} />
-            </div>
-        </div>)
+  const productElement = products?.map((product) => (
+    <div className="card" key={product.name}>
+      <img className="special-image" src={product.image} alt={product.name} />
+      <div className="card-body">
+        <span className="price">
+          {(product.price).toLocaleString('en-UK', {
+            style: 'currency',
+            currency: 'GBP',
+          })}
+        </span>
+        <Quontity onAdd={onAdd} onRemove={onRemove} product={product} />
+      </div>
+    </div>
+  ));
 
   return (
     <div className="container">
-        <div className='row row-cols-4 row-cols-md-3'>
-            {productElement}
-        </div>
-        <div>Total: <span>{total}</span></div>
-        <div>Number of items in card: {cartItems.length}</div>
+      <div className="row row-cols-4 row-cols-md-3">
+        {productElement}
+      </div>
+      <div>
+        Total:
+        <span>{total}</span>
+      </div>
+      <div>
+        Number of items in card:
+        {cartItems.length}
+      </div>
     </div>
   );
 }
--- src/Quontity.js
+++ src/Quontity.js
@@ -1,38 +1,37 @@
-import { useCallback, useEffect, useState } from "react";
-
+import { useState } from 'react';
 
 function Quantity(props) {
-    const { onAdd, onRemove, product} = props
+  const { onAdd, onRemove, product } = props;
 
-    const handleAddButton = () => {
-        onAdd(product)
-        setAmount(amount + 1)
+  const handleAddButton = () => {
+    onAdd(product);
+    setAmount(amount + 1);
+  };
+  const handleRemoveButton = () => {
+    onRemove(product);
+    if (amount !== 0) {
+      setAmount(amount - 1);
     }
-    const handleRemoveButton = () => {
-        onRemove(product)
-        if (amount !== 0) {
-            setAmount(amount - 1)
-        }
-    }
+  };
 
-    const [amount, setAmount] = useState(0)
+  const [amount, setAmount] = useState(0);
 
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <span>Quantity</span>
-                </div>
-                <div className="col">
-                    <strong>{amount}</strong>
-                </div>
-                <div className="col">
-                    <button type='button' className='special-button' onClick={() => handleAddButton()}>+</button>
-                    <button type='button' className='special-button' onClick={() => handleRemoveButton()}>-</button>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <span>Quantity</span>
         </div>
-)
+        <div className="col">
+          <strong>{amount}</strong>
+        </div>
+        <div className="col">
+          <button type="button" className="special-button" onClick={() => handleAddButton()}>+</button>
+          <button type="button" className="special-button" onClick={() => handleRemoveButton()}>-</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Quantity
+export default Quantity;
